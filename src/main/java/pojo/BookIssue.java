53795c7b252c1776b0a91c665340005437ea2b3f
Khoa <dangkhoa123bn@gmail.com>
2021-12-02 13:57:00 +0700

    create setter and getter

--- src/main/java/pojo/BookIssue.java
+++ src/main/java/pojo/BookIssue.java
@@ -1,4 +1,40 @@
 package pojo;
 
+import java.util.Date;
+
 public class BookIssue {
+    private int id,userId;
+    private Date date,returnDueDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getReturnDueDate() {
+        return returnDueDate;
+    }
+
+    public void setReturnDueDate(Date returnDueDate) {
+        this.returnDueDate = returnDueDate;
+    }
 }
--- src/main/java/pojo/IssueDetail.java
+++ src/main/java/pojo/IssueDetail.java
@@ -1,5 +1,41 @@
 package pojo;
 
-public class IssueDetail {
+import java.util.Date;
 
+public class IssueDetail {
+    private int bookId,issueId;
+    private Date returnDate;
+    private String bookState;
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getIssueId() {
+        return issueId;
+    }
+
+    public void setIssueId(int issueId) {
+        this.issueId = issueId;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getBookState() {
+        return bookState;
+    }
+
+    public void setBookState(String bookState) {
+        this.bookState = bookState;
+    }
 }
