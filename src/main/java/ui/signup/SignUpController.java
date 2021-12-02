c4c9fd224bcf9d27d67d271ab72c8a7387e35607
giahuyoke <giahuyoke01@gmail.com>
2021-12-02 22:28:11 +0700

    Revert "Hello"
    This reverts commit 261a8daddff3e25072810301ed7c37b487b6cb53.

--- src/main/java/ui/signup/SignUpController.java
+++ src/main/java/ui/signup/SignUpController.java
@@ -1,6 +1,9 @@
 package ui.signup;
 
-import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -12,9 +15,8 @@ public class SignUpController implements Serializable {
     @FXML private JFXTextField txtId;
     @FXML private JFXPasswordField txtPw;
     @FXML private JFXButton btnLogin, btnSignUp;*/
-    @FXML private JFXCheckBox Female;
-    @FXML
 
+    @FXML
     public void btnCreateHandler() {
 
     }
