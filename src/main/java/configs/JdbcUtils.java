9c4b2aeac12503d00b69e96597d8a9dfda1a0570
Nguyen Cong Tam <1951012112tam@ou.edu.vn>
2021-12-02 12:00:45 +0700

    change mysql password

--- src/main/java/configs/JdbcUtils.java
+++ src/main/java/configs/JdbcUtils.java
@@ -14,6 +14,6 @@ public class JdbcUtils {
     }
     public static Connection getConn() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://localhost/librarydb",
-                "root", "nazigermany");
+                "root", "12345678");
     }
 }
